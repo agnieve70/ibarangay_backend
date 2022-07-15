789c03bf0b2c0358e6c55cf6370774c4458c4e1a
agnieve <ag@castledigital.com.au>
2022-07-15 15:59:28 +0800

    added fixes

--- app/Http/Controllers/Api/ReportController.php
+++ app/Http/Controllers/Api/ReportController.php
@@ -29,7 +29,7 @@ class ReportController extends Controller
     function getDetail($id){
         $report = Report::select('report.id','report.title as report_title', 'latitude', 'longitude','report.content as report_content','category','report.status',
         'representative_id', 'help_category.title as category', 'report.created_at', 'report.updated_at',
-        DB::raw('(SELECT name from users WHERE users.id = help.user_id) as `helped_user`'))
+        DB::raw('(SELECT CONCAT("firstname", " ", "lastname") from users WHERE users.id = help.user_id) as `helped_user`'))
         ->join('help', 'help.id', 'report.help_id')
         ->join('help_category', 'help_category.id', 'report.category')
         ->where('report.id', $id)
